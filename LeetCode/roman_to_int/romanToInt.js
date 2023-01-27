86ed40aeb5ccaef7d40713db60a9f174ac5150e1
Ibnballo1 <belloabdullateef035@gmail.com>
2023-01-27 17:33:11 +0100

    Add leetcode file

---
+++ LeetCode/roman_to_int/romanToInt.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {};
+
+console.log(romanToInt('III')); // 3
+console.log(romanToInt('LVIII')); // 58
+console.log(romanToInt("MCMXCIV")); // 3
