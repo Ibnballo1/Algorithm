a30a1dc0532554aa225d2944a8fec97ec03aa64c
Ibnballo1 <belloabdullateef035@gmail.com>
2023-02-05 17:50:20 +0100

    Solve sales by match

--- HackerRank/SalesByMatch/salesByMatch.js
+++ HackerRank/SalesByMatch/salesByMatch.js
@@ -9,6 +9,19 @@
 
 function sockMerchant(n, ar) {
   // Write your code here
+  let result = 0;
+  let finalResult = 0;
+  const sortedArr = ar.sort();
+  for (let i = 0; i < n; i++) {
+    if (ar[i] === sortedArr[i]) {
+      result += 1
+      if (ar[i] !== ar[i + 1]) {
+        finalResult += Math.floor(result / 2);
+        result = 0
+      }
+    }
+  }
+  console.log(finalResult);
 }
 
 sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]); // => 3
