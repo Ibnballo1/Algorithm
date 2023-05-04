007ae1889c303d58b2d508e936fde37c6448493d
Ibnballo1 <belloabdullateef035@gmail.com>
2023-05-04 18:47:17 +0100

    Update two-sum

--- LeetCode/two-sum/two-sum-2.js
+++ LeetCode/two-sum/two-sum-2.js
@@ -1,8 +1,17 @@
 function twoSum(nums, target) {
-  nums.reduce((total, element) => {
-    console.log(element);
-    return total;
-  })
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        result.push(i, j);
+      }
+    }
+  }
+  return result;
 }
 
-console.log([2, 4, 5, 8], 10); // Output: 
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [ 0, 1 ]
+console.log(twoSum([3, 2, 4], 6)); // Output: [ 1, 2 ]
+console.log(twoSum([3, 3], 6)); // Output: [ 0, 1 ]
+console.log(twoSum([3, 2, 3], 6)); // Output: [ 0, 2 ]
+console.log(twoSum([2, 5, 5, 7], 10)); // Output: [ 1, 2 ]
--- LeetCode/two-sum/two-sum.js
+++ LeetCode/two-sum/two-sum.js
@@ -4,24 +4,22 @@
  * @return {number[]}
 */
 var twoSum = function(nums, target) {
-  // the input is an int, array of integer
-  // the output is also int
-  // I want to return the indices of the addition
-  // of two elements from the nums array
-  // that will be === to the target
-  let result = []
-
-  // first i need to loop through the array to add each element with other element
+  let map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    // Now I will check if the addition of any 2 elements == to the target
-    if (nums[i] + nums[i + 1] == target) {
-      // return their index
-      result.push(i, i + 1)
-      return result;
+    map.set(target - nums[i], i);
+  }
+  for (let i = 0; i < nums.length; i++) {
+    let value = map.get(nums[i]);
+    console.log('Value is: ', value)
+    if (value !== undefined && value !== i) {
+      return [i, value];
     }
   }
-};
+}
 
-console.log(twoSum([2, 7, 11, 15], 9)); // Output: [ 0, 1 ]
-console.log(twoSum([3, 2, 4], 6)); // Output: [ 1, 2 ]
-console.log(twoSum([3, 3], 6)); // Output: [ 0, 1 ]
+
+// console.log(twoSum([2, 7, 11, 15], 9)); // Output: [ 0, 1 ]
+// console.log(twoSum([3, 2, 4], 6)); // Output: [ 1, 2 ]
+// console.log(twoSum([3, 3], 6)); // Output: [ 0, 1 ]
+console.log(twoSum([3, 2, 3], 6)); // Output: [ 0, 2 ]
+// console.log(twoSum([2, 5, 5, 7], 10)); // Output: [ 1, 2 ]
